ab798cac4a23c13ac4deb5a46b0d5e0a39de44ff
Ian Vaughan <github@ianvaughan.co.uk>
2013-06-01 23:58:02 +0100

    read files in

--- lib/import.rb
+++ lib/import.rb
@@ -2,23 +2,23 @@ require 'json'
 
 module Rentify
   class Import
-    def self.import input
-      # read = read_data input
+    def self.import data_or_filename
+      input = read_data data_or_filename
       data = []
 
-      input.split(/Flat ./).each_with_index do |line, i|
+      input.split(/Flat [0-9]/).each_with_index do |line, i|
         next if line.nil? || line['name'].nil?
-        data << {id: "Flat #{i}"}.merge(JSON.parse("{#{line}}", :symbolize_names => true))
+        json = JSON.parse("{#{line}}", :symbolize_names => true)
+        data << {id: "Flat #{i}"}.merge(json)
       end
       data
     end
 
     private
 
-    # def read_data source
-    #   return source.read if source.respond_to?(:read)
-    #   return File.read(source.to_str) if source.respond_to?(:to_str)
-    #   raise ArgumentError
-    # end
+    def self.read_data source
+      return File.read(source.to_str) if File.exists? source
+      source
+    end
   end
 end
--- spec/lib/import_spec.rb
+++ spec/lib/import_spec.rb
@@ -4,16 +4,14 @@ require 'fakefs/spec_helpers'
 
 module Rentify
   describe Import do
-    include FakeFS::SpecHelpers
-
-    subject { described_class.import(file) }
+    # include FakeFS::SpecHelpers
+    # subject { described_class.import(file) }
 
     let(:expected) { [
       {id: "Flat 1", name: "A house", bedroom_count: 3, latitude: 51.501000, longitude: -0.142000},
       {id: "Flat 2", name: "a flat", bedroom_count: 2, latitude: 51.523778, longitude: -0.205500},
       ] }
 
-    context 'parsing data' do
       let(:file) { %|Flat 1
 "name": "A house",
 "bedroom_count": 3,
@@ -27,13 +25,14 @@ Flat 2
 "longitude": -0.205500
 |}
 
-      it { should eq expected }
+    context 'parsing data' do
+      it { Import.import(file).should eq expected }
     end
 
     context 'given a filename' do
-      # before { FileUtils.expects(:mkdir).with("directory").once }
-      # let(:file) {  }
-      # it { should eq expected }
+      let(:filename) { './import/data.txt' }
+      before { File.should_receive(:read).with(filename).once.and_return(file) }
+      it { Import.import(filename).should eq expected }
     end
   end
 end
