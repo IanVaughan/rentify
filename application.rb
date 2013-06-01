0c502dcfed6e2df7568213741c4bd21a8288c3d3
Ian Vaughan <github@ianvaughan.co.uk>
2013-06-01 12:09:39 +0100

    basic sinatra server

---
+++ application.rb
@@ -0,0 +1,2 @@
+require 'sinatra'
+load File.join(File.dirname(__FILE__), 'environment.rb')
---
+++ spec/app/server_spec.rb
@@ -0,0 +1,26 @@
+# require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+require './app/server'
+require 'sinatra'
+require 'spec_helper'
+require 'rack/test'
+
+# setup test environment
+set :environment, :test
+set :run, false
+set :raise_errors, true
+set :logging, false
+
+describe 'Server' do
+  include Rack::Test::Methods
+
+  def app
+    Server
+  end
+
+  it "gets root" do
+    get '/'
+    last_response.should be_ok
+    last_response.body.should == 'hello' # last_response.body.include?('hello')
+  end
+end
